d89b22b9c6e0354d2fbff6271038c2847d0d6386
captproton <carl@wdwhub.net>
2023-01-11 12:19:58 -0800

    INIT: jumpstart

---
+++ config/routes/madmin.rb
@@ -0,0 +1,17 @@
+# Below are the routes for madmin
+namespace :madmin do
+  resources :services
+  resources :notifications
+  resources :announcements
+  resources :users
+  namespace :active_storage do
+    resources :variant_records
+  end
+  namespace :active_storage do
+    resources :blobs
+  end
+  namespace :active_storage do
+    resources :attachments
+  end
+  root to: "dashboard#show"
+end
