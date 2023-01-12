ce9ed17090cb3d7a4817bd6cadc5f837c3dacb53
captproton <carl@wdwhub.net>
2023-01-11 16:57:33 -0800

    ADD: vite_ruby and windicss

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import WindiCSS from 'vite-plugin-windicss'
+import FullReload from 'vite-plugin-full-reload'
+import RubyPlugin from 'vite-plugin-ruby'
+
+export default defineConfig({
+  plugins: [
+    WindiCSS({
+      root: __dirname,
+      scan: {
+        fileExtensions: ['erb', 'haml', 'html', 'vue', 'js', 'ts', 'jsx', 'tsx'],
+        dirs: ['app/views', '../frontend'], // or app/javascript, or app/packs
+      },
+    }),
+    FullReload(['config/routes.rb', 'app/views/**/*']),
+    RubyPlugin(),
+  ],
+})
