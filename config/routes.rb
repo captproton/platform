91df9d99a773374cfca45d58f01dce88117d97d2
captproton <carl@wdwhub.net>
2023-01-26 19:50:58 -0800

    CHANGE: put avo behind user sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   resources :posts
-  mount Avo::Engine, at: Avo.configuration.root_path
   resources :projects
   resources :accounts
   namespace :backstage do
@@ -13,9 +12,12 @@ Rails.application.routes.draw do
   draw :madmin
   get '/privacy', to: 'home#privacy'
   get '/terms', to: 'home#terms'
+  authenticate :user do
+    mount Avo::Engine, at: Avo.configuration.root_path
+  end
   authenticate :user, lambda { |u| u.admin? } do
     mount Sidekiq::Web => '/sidekiq'
-
+    
     namespace :madmin do
       resources :impersonates do
         post :impersonate, on: :member
@@ -30,7 +32,8 @@ Rails.application.routes.draw do
   resources :notifications, only: [:index]
   resources :announcements, only: [:index]
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-    root to: 'home#index'
+
+  root to: 'home#index'
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
     # Defines the root path route ("/")
